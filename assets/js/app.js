7857171883daf66542c341f69bcef7a9807e54bb
Kevin <k.oreel@student.avans.nl>
2021-09-21 09:48:28 +0200

    Solution1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
 console.log('JavaScript is working!')
+
+var c = document.getElementById("myCanvas");
+var diamond = c.getContext("2d");
+diamond.moveTo(0, 50);
+diamond.lineTo(100, 100);
+diamond.lineTo(200, 50);
+diamond.lineTo(100, 0);
+diamond.lineTo(0, 50);
+diamond.stroke();
