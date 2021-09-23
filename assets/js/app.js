06970b20d3ca272cc32d74a1c1a76651fcb945ca
Kevin <k.oreel@student.avans.nl>
2021-09-23 12:28:02 +0200

    Made Lesson2 exercises and assignments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,45 @@
 console.log("JavaScript is working!");
+
+// Exercise 1
+let firstName = "Kevin";
+let lastName = "Oreel";
+let age = 20;
+let sentence = "Hello, I am " + firstName + " " + lastName +  " and I am " + age + " years old.";
+console.log(sentence);
+
+
+// Exercise 2
+let priceOfAnApple = '0.59';
+let numberOfApples = 200;
+console.log(priceOfAnApple * numberOfApples);
+
+
+// Exercise 3
+let a = 5;
+let b = 6;
+let c = undefined;
+console.log(c = Math.sqrt((a * a) + (b * b)))
+
+
+// Exercise 4
+const hzSentence = "Please visit the HZ University of Applied Sciences";
+console.log(hzSentence.replace("Applied", "Mad"));
+
+
+// Assignment 1
+function isEven(number) {
+    if(number % 2 === 0) {
+        console.log("Number is even")
+    } else {
+        console.log("Number is not even")
+    }
+}
+
+
+// Assignment 2
+let secondAssignment = 'Programming is not so cool';
+console.log(secondAssignment.replace(" not", ""));
+
+
+// Assignment 3
+// See bouncing balls folder
