729604f01f925dcfa1eb3c634820d7891abe575b
Kevin <k.oreel@student.avans.nl>
2021-10-05 09:31:00 +0200

    Assignments 1 and 2 of Lesson5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,26 @@
 console.log("JavaScript is working!");
 
 // Assignment 1
-let highestNumber = 400;
-let lowestNumber = 100;
-
-while (highestNumber >= lowestNumber) {
-  if (highestNumber % 3 == 0) {
-    console.log(highestNumber);
-    highestNumber--;
-  } else {
-      highestNumber--;
-  }
+const year = prompt("Enter current year", "2021")
+function happyNewYear(year) {
+    for (i=10; i>=0; i--) {
+        console.log(i);
+    }
+    console.log(`Happy new ${year}!`)
 }
 
-// Assignment 2
-// Code snippet from bouncing ball project
-let maxRadius = 15;
-let colour = "red";
-let radius = Math.floor(Math.random() * maxRadius);
-let xVelocity = 5; // the speed of a ball on the x-axis
-let yVelocity = 6; // the speed of a ball on the y-axis
 
-for (let i = 0; i < 5; i++) {
-  createNewBall(radius, "green", xVelocity, yVelocity);
+// Assignment 2
+function factorial(num) {
+    let output = 1;
+
+    for (i=1; i<=num; i++) {
+        output *= i; 
+    }
+    return output;
 }
 
 // Assignment 3
-let niceNumber = [2, 4, 8, 9, 12, 13];
-let total = 0;
+function checkForACharacterInSentence(sentence, character) {
 
-for (i = 0; i < niceNumber.length; i++) {
-  total += niceNumber[i];
 }
-console.log(total);
