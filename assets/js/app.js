2538c6ec72f8091445e8b8610d9ed5c9d2d43aa8
Kevin // Zirconics <56792130+Zirconics@users.noreply.github.com>
2021-10-24 18:34:01 +0200

    Made first excercise of Lesson9's homework

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,6 +29,31 @@ const songs = [
 	'Fire starter'
 ];
 
+// Logs the clicked song in the console
+function logSong(event) {
+  if (event.target.nodeName === "LI") {
+    console.log("You clicked on: " + event.target.innerText);
+  }
+  
+}
+
+// Create a div and a list.
+listDiv = document.createElement('div')
+listOfSongs = document.createElement('ul')
+
+// Add all components to the page
+document.getElementsByTagName('body')[0].appendChild(listDiv);
+listDiv.appendChild(listOfSongs);
+
+//loop through all the songs and add them to the list element
+for (i=0; i<songs.length; i++) {
+  listItem = document.createElement('li');
+  listItem.innerHTML = songs[i];
+  // Makes the list items clickable and prints the song name in the console.
+  listItem.addEventListener("click", logSong)
+  listOfSongs.appendChild(listItem);
+}
+
 
 /*
 Assignment 2
