f20179095d14fff54208cd6e092816c0b4e83ebc
Kevin // Zirconics <56792130+Zirconics@users.noreply.github.com>
2021-10-24 15:05:19 +0200

    Completed homework of Lesson8

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,39 +2,38 @@ console.log("JavaScript is working!");
 
 /*
 In Class assignments
+Works only in pricing.html
 */
 
-const pricingBox = document.querySelector("#pricing-box")
-const pricingBox2 = document.getElementById("pricing-box")
-const sections = document.getElementsByTagName('section')
-const tempSectionHeader = document.querySelector("#section-2 > .card-header")
-const roleItems = document.querySelectorAll("[role]")
-const storageItems = document.querySelectorAll("li[data-description-value='storage']")
+// const pricingBox = document.querySelector("#pricing-box")
+// const pricingBox2 = document.getElementById("pricing-box")
+// const sections = document.getElementsByTagName('section')
+// const tempSectionHeader = document.querySelector("#section-2 > .card-header")
+// const roleItems = document.querySelectorAll("[role]")
+// const storageItems = document.querySelectorAll("li[data-description-value='storage']")
 
 
-console.log(pricingBox.innerHTML)
-console.log(pricingBox.textContent)
+// console.log(pricingBox.innerHTML)
+// console.log(pricingBox.textContent)
 
-for (i=0; i<storageItems.length; i++) {
-  console.log(storageItems[i].textContent)
-}
+// for (i=0; i<storageItems.length; i++) {
+//   console.log(storageItems[i].textContent)
+// }
 
-const paragraph = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt \
-  iste magni veniam pariatur incidunt esse dolore molestiae impedit, id natus est modi \
-  eveniet maiores obcaecati optio dolores iure repellendus unde?";
-const assignment4 = document.querySelector("#pricing-box > .fs-5")
-assignment4.textContent = paragraph;
+// const paragraph = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt \
+//   iste magni veniam pariatur incidunt esse dolore molestiae impedit, id natus est modi \
+//   eveniet maiores obcaecati optio dolores iure repellendus unde?";
+// const assignment4 = document.querySelector("#pricing-box > .fs-5")
+// assignment4.textContent = paragraph;
 
-console.log(assignment4)
+// console.log(assignment4)
 
-const toBeDeleted = document.getElementById("section-1").closest("div > div").remove()
-console.log(toBeDeleted)
+// const toBeDeleted = document.getElementById("section-1").closest("div > div").remove()
+// console.log(toBeDeleted)
 
 
 /*
 Feedback assignments
-
-
 Assignment 1
 Given is this table. 
 - Using JavaScript, calculate the average of all the grades from the different courses.
@@ -49,6 +48,19 @@ Hint: you can change the HTML if you think that is a smart thing to do.
 </table>
 */
 
+
+  var table = document.getElementById('gradeTable');
+  let column = 1;
+  let sum = 0;
+
+  for (i = 0; i < table.rows.length; i++) {
+      if (table.rows[i].cells.length > column) { 
+          td = table.rows[i].cells[column];      
+          sum += (parseFloat((td.innerText)))
+      } 
+  }
+
+console.log("The sum of all grades is: " + sum)
 
 
 /*
@@ -66,6 +78,12 @@ Hint: you can change the HTML if you think that is a smart thing to do.
 	<li class="course">Framework development 1</li>
 </ul>
 */
+let row = document.getElementById('courses')
+for (i=0; i<row.children.length; i++) {
+  if (i == 0 || i % 2 == 0) {
+    row.children[i].style.backgroundColor = 'red';
+  }
+}
 
 
 /*
@@ -73,6 +91,13 @@ Assignment 3
 Create a function `createImageElement(imageSrcName)` which creates an HTML image element in JavaScript and add it to the DOM. 
 For images you can use [http://lorempixel.com/](http://lorempixel.com/)
 */
+function createImageElement(imageSrcName) {
+  let newImg = document.createElement('img')
+  newImg.src = imageSrcName
+  document.getElementById('imageDiv').appendChild(newImg);
+}
+
+createImageElement('https://picsum.photos/200/300')
 
 
 /*
@@ -96,3 +121,7 @@ Write a function that finds all the checkboxes and writes the id's to the consol
   </div>
 </div>
 */
+let checkboxes = document.getElementById('form')
+for (k=0; k<checkboxes.children.length; k++) {
+  console.log("ID #" + k +" is: " + checkboxes.children[k].children[0].id)
+}
